e4ce3f365cafc8d30df46e57d788989d63248c85
KwonSoungHo <soung091@naver.com>
2021-07-01 00:36:19 +0900

    Merge branch 'develop' of https://github.com/soung0911/git-test into develop
    # Conflicts:
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -18,23 +18,7 @@ function App() {
           Learn React 1.1-header 작업완료 1.1-section 작업완료 1.2-nav 작업완료
           1.2-footer 작업완료 1.2-nav 33 작업완료 check1 check2
           11111111111111111 33333333333333333 22222222 a기능입니ㅏㄷ!!!! bbbbbb
-          기능입니다! g기능입니다!!! d기능을만듭니다!! test
-
-
-
-          test2
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-          test4
-=======
-
-          test3
->>>>>>> 866de8d (test3)
-=======
-
-          test3
->>>>>>> 866de8d6f0c11315ef38766194aea3523031d757
+          기능입니다! g기능입니다!!! d기능을만듭니다!! test test2 test4 test3
         </a>
       </header>
     </div>
